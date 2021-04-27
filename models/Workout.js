1c85f1e8e275a76fb0ec19aac9be02fb04a33909
G Code <seanlfrancis@gmail.com>
2021-04-27 15:44:13 -0500

    mongoose models done

---
+++ models/Workout.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workout = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+            required: "Must enter a type"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Must enter a type"
+        },
+        duration: {
+            type: Number,
+            required: "Please enter a duration"
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        },
+    },
+    ],
+});
+
+const Workout = mongoose.Model("Workout", workout);
+module.exports = Workout;
