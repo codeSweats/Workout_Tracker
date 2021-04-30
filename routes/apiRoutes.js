00db342c75fbcd9e7458227af69e343fe953cbf9
G Code <seanlfrancis@gmail.com>
2021-04-29 23:17:55 -0500

    Finished htmlRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
+const path = require("path");
+const { Workout } = require('../models/Workout.js');
 
+
+router.get()
 
 
 module.exports = router; 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const path = require("path");
 
+// Makes a get request to the target '/exercise', then delivers the exercise.html to that endpoint
+router.get('/exercise', (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, '../public/exercise.html'))
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
+// Makes a get request to the target '/stats', then delivers the stats.html client-side
+router.get('/stats', (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, '../public/stats.html'))
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
-module.exports = router; 
-
-
+module.exports = router;
