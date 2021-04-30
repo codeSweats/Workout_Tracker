19e4d74a142005c923e4013e956e93eca8c45071
G Code <seanlfrancis@gmail.com>
2021-04-30 16:06:53 -0500

    Completed the apiRoutes. App is working well.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,51 @@
 const router = require('express').Router();
-const path = require("path");
-const { Workout } = require('../models/Workout.js');
+const Workout = require('../models/Workout.js');
 
 
 
+router.post("/workouts", (req, res) => {
+    console.log("route hit");
+    Workout.create({})
+    .then((workoutdb) => {
+        console.log(workoutdb, "This is from post route")
+        res.json(workoutdb);
+    }).catch((err) => {
+        console.err(err)
+    })
+});
+
+router.put("/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, 
+        {$push: {exercises: req.body}})
+        .then((workoutdb) => {
+            console.log("This is from the put route");
+            res.json(workoutdb);
+        })
+});
+
+router.get("/workouts", (req, res) => {
+    Workout.aggregate([{
+        $addFields: {
+            totalDuration: {
+                $sum: "$exercises.duration"
+            }
+        }
+    }]).then((workoutdb) => {
+        res.json(workoutdb)
+    }) 
+});
+
+router.get("/workouts/range", (req, res) => {
+    Workout.aggregate([{
+        $addFields: {
+            totalDuration: {
+                $sum: "$exercises.duration"
+            }
+        }
+    }]).then((workoutdb) => {
+        res.json(workoutdb)
+    }) 
+});
 
 
 module.exports = router; 
